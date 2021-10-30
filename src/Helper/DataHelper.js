ad97142f95023a68184fb90a08f94d651ebe7e96
Yanbin <zhongyanbin1992@gmail.com>
2021-10-30 19:21:37 +0800

    fix

--- src/Helper/DataHelper.js
+++ src/Helper/DataHelper.js
@@ -25,14 +25,14 @@ export const contentListNoTitle = {
 
 
 export const TestQuestion = [
-  { title: "test1", content: "contesnt 1111>>>>>>>", a: "this is a", b: "this is b", c: "this is c", d: "this is d" },
-  { title: "test2", content: "contesnt 2222>>>>>>>", a: "this is a", b: "this is b", c: "this is c", d: "this is d" },
-  { title: "test3", content: "contesnt 3333>>>>>>>", a: "this is a", b: "this is b", c: "this is c", d: "this is d" },
-  { title: "test4", content: "contesnt 4444>>>>>>>", a: "this is a", b: "this is b", c: "this is c", d: "this is d" },
-  { title: "test5", content: "contesnt 5555>>>>>>>", a: "this is a", b: "this is b", c: "this is c", d: "this is d" },
-  { title: "test6", content: "contesnt 6666>>>>>>>", a: "this is a", b: "this is b", c: "this is c", d: "this is d" },
-  { title: "test7", content: "contesnt 7777>>>>>>>", a: "this is a", b: "this is b", c: "this is c", d: "this is d" },
-  { title: "test8", content: "contesnt 8888>>>>>>>", a: "this is a", b: "this is b", c: "this is c", d: "this is d" },
-  { title: "test9", content: "contesnt 9999>>>>>>>", a: "this is a", b: "this is b", c: "this is c", d: "this is d" },
-  { title: "test10", content: "contesnt 1010>>>>>>>", a: "this is a", b: "this is b", c: "this is c", d: "this is d" },
+  { title: "Question 1", content: "What is your annual income?", a: "Less than 50K (SGD)", b: "Between 50K - 100K (SGD)", c: "Between 100K - 150K (SGD)", d: "More than 150K (SGD)" },
+  { title: "Question 2", content: "How much of your income do you plan to invest?", a: "Less than 10%", b: "Between 10% - 25%", c: "Between 25% - 50%", d: "More than 50%" },
+  { title: "Question 3", content: "Your main source of income is?", a: "Wages and remuneration for labor services", b: "Financial asset income such as interest, dividends, and transfer", c: "Income from non-financial assets such as lease and sale of real estate", d: "No fixed income" },
+  { title: "Question 4", content: "How many of the following products have you come into contact with (Stock, Funding, Bank products, and insurance)?", a: "More than 3", b: "Between 2 - 3", c: "Only 1", d: "None" },
+  { title: "Question 5", content: "How many years have you been in wealth management?", a: "Less than 1 year", b: "1 - 3 years", c: "3 - 5 years", d: "more than 5 years" },
+  { title: "Question 6", content: "What is your planned investment period?", a: "Less than 1 month", b: "1 - 6 months", c: "6 - 12 months", d: "this is d" },
+  { title: "Question 7", content: "Which of the following descriptions best fits your investment attitude?", a: "Is risk averse, does not want to lose the principal, and hopes to obtain a stable return ", b: "Is conservative investment, does not want to lose the principal, and is willing to bear a certain amount of income Interest volatility", c: "Looking for higher returns and growth of funds, willing to bear a limited principal loss for this purpose", d: "Hope to earn high returns, willing to undertake this Bear a large loss of principal" },
+  { title: "Question 8", content: "Assume that there are two kinds of investments: Investment A is expected to obtain a 10% return, and the possible loss is very small; Investment B is expected to receive 30% of the return but may bear larger losses. What will you do with your investment:", a: "Invests all in A with lower returns and lower risks", b: "Invests in both A and B, but most of the funds are invested in A with lower returns and lower risks", c: "Invests in A and B at the same time, but most of the funds are invested in B with higher returns and higher risks", d: "All invested in B with higher returns and higher risks" },
+  { title: "Question 9", content: "What is the biggest investment loss you think you can bear?", a: "Within 10%", b: "10% - 30%", c: "30% - 50%", d: "More than 50%" }
+
 ]
--- src/containers/EntryTest/test.actions.js
+++ src/containers/EntryTest/test.actions.js
@@ -1,6 +1,9 @@
 import {
-  POST_TEST_RESULT,
+  POST_ENTRY_TEST,
+  POST_ENTRY_TEST_SUCCESS,
+  POST_ENTRY_TEST_FAILURE
 } from './test.constants';
 
-export const postTestResultAction = payload => ({ type: POST_TEST_RESULT, payload });
-
+export const postEntryTestAction = payload => ({ type: POST_ENTRY_TEST, payload });
+export const postEntryTestSuccess = payload => ({ type: POST_ENTRY_TEST_SUCCESS, payload });
+export const postEntryTestFailure = payload => ({ type: POST_ENTRY_TEST_FAILURE, payload });
